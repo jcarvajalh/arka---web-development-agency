251bfd40ec86c0e46a0392f578e8a1113ede1692
jcarvajalh <caamiloc@outlook.com>
2026-06-23 10:31:07 -0500

    nueva url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'arka-agency.pages.dev',
+  site: 'https://arka.arka-agency.pages.dev',
 
   vite: {
     plugins: [tailwindcss()],
