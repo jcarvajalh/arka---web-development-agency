ce444bde4d5031ccc852b2c6d0729a24e23e809b
jcarvajalh <caamiloc@outlook.com>
2026-06-23 10:30:12 -0500

    modificar astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://arka-agency.vercel.app/',
+  site: 'arka-agency.pages.dev',
 
   vite: {
     plugins: [tailwindcss()],
